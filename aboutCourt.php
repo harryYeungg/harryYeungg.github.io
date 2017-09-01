6e1163c3eb2eed34c504cc9dbfd5b304b698d19b
harryYeungg <31332033+harryYeungg@users.noreply.github.com>
2017-08-31 21:42:37 -0700

    Reupload ver

---
+++ aboutCourt.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" >
+	<title>About | Courtsearch</title>
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	  <link href='https://fonts.googleapis.com/css?family=Rajdhani' rel='stylesheet'>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<style type="text/css">
+		@import url(http://fonts.googleapis.com/css?family=Raleway:700,400);
+
+		body {			
+			background-image: linear-gradient(to bottom, rgba(0,0,0,0.2) ,rgba(0,0,0,0.2) ), url("30 Basketball Backgrounds Wallpapers Images Pictures  Design ....jpg");
+			background-size: cover;
+			background-repeat: no-repeat;
+		    background-attachment: fixed;
+		    background-position: center;
+		   	color: white;
+		   	font-family: "Rajdhani", cursive, sans-serif;
+
+		}
+
+		.header {
+			margin-top: 20px;
+		}
+
+		li {
+			display: inline;
+			margin-right: 20px;
+
+		}
+
+		ul {
+			padding-top: 16px;
+			
+		}
+
+		ul  a {
+			text-decoration: none;
+			color: white;
+		}
+
+		ul  a:hover {
+			color: orange;
+			text-decoration: none;
+		}
+
+		h3 a {
+			text-decoration: none;
+			color: white;
+		}
+		h3 a:hover {
+			cursor: pointer;
+			text-decoration: none;
+			color: white;
+		}
+
+		.termsandprivacy  {
+			color: white;
+			text-decoration: none;
+		}
+		.termsandprivacy:hover {
+			color: orange;
+			text-decoration: none;
+			
+		}
+
+		#founderList li {
+			display: inline;
+		}
+
+		.nav li a:hover {
+			background-color: transparent;
+		}
+
+
+
+	
+
+	</style>
+</head>
+
+<body>
+
+
+
+
+
+	
+	<!-- Creating the header as well as the nav bar (for non mobile device)-->
+	<div class = "container header" ">
+		<div class = row>
+			<div class = "hidden-xs col-sm-4 col-md-4 col-lg-4" >
+				<!--<h3><a href ="indexCourt.html">Court<span style = "color: orange;">Search</span></a></h3>-->
+				<a href="index.php"><img src="bball final 6.png" width="220" height="130"  ></a>
+			</div>
+			<div class = "hidden-xs col-sm-offset-3 col-sm-5 col-md-offset-3 col-md-5 col-lg-offset-3 col-lg-5" style="text-align: right; margin-top: 31px;">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href = "aboutCourt.php">About</a></li>
+					
+					<!--<li><button class = "btn btn-warning btn-small" style="border-radius: 50px; border-style: none;">Sign up</button></li>-->
+				</ul>
+			</div>
+
+		</div>
+	</div>
+
+
+	<!-- Creating the header as well as the nav bar (for mobile device)-->
+
+
+  <nav class="navbar hidden-sm hidden-md hidden-lg">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar" style="background-color: #BDC3C7 ; margin-top: 15px;">
+        <span class="icon-bar" style="background-color: white;"></span>
+        <span class="icon-bar" style="background-color: white;"></span>
+        <span class="icon-bar" style="background-color: white;"></span>
+      </button>
+      <a href="index.php"><img src="bball final 6.png" width="130" height="80"  style="margin-left: 10px;"></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.php">Home</a></li>
+        <li><a href="aboutCourt.php">About</a></li>
+        
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+
+
+	<!-- Creating about content-->
+
+<div class = container style="text-align: center;">
+<div class = row> 
+<div class = "col-sm-offset-5 col-sm-7 col-md-7 col-lg-7">
+<h3>About Us</h3>
+<p> The idea of CourtSearch originated from the will to know if one streetball court has been full of players or not without going there in person. Here in CourtSearch, we offer you real-time  information of a court, such as the number of players playing as well as the photo updates of the court. We also try to connect you with your nearby friends and play a pickup game instantly. And that's the way to truly enjoy basketball. </p>
+
+<h3 style="margin-top: 50px;">Meet the team</h3> 
+</div>
+</div>
+<div class = row>
+<div class = "hidden-xs col-sm-offset-5 col-sm-7 col-md-7 col-lg-7">
+<ul id = founderList>
+	<li id=yhc style="margin-right: 100px;"><span class="glyphicon glyphicon-user" style="margin-right: 5px;"></span>Co-founder: HC YEUNG</li>
+	<li ><span class="glyphicon glyphicon-user" style="margin-right: 5px;"></span>Co-founder: PH LAM</li>
+</ul>
+</div>
+
+<div class = "hidden-sm hidden-md hidden-lg">
+ <ul id=founderList2>
+ <li><span class="glyphicon glyphicon-user" style="margin-right: 5px;"></span>Co-founder: HC YEUNG</li>
+ <li><span class="glyphicon glyphicon-user" style="margin-right: 5px;"></span>Co-founder: PH LAM</li>
+
+
+ </ul>
+
+</div>
+</div>
+</div>
+	
+	
+	
+
+	<!-- Creating the footer-->
+
+	<footer  class = "hidden-xs hidden-md hidden-sm" style="margin-top: 220px;" >
+		
+			<div class = row style="text-align: center;">
+				<h6>&#9400 2017 Courtionary.Co</h6>
+			</div>
+
+			<div class = row style="text-align: center; margin-top: 0px;">
+				
+				<h6 style = "margin-top: 1px;"> <a href="#" class = termsandprivacy>Terms</a> | <a href="#" class = termsandprivacy>Privacy</a> </h6>
+			</div>
+		
+	</footer>
+
+
+	<footer style="margin-top: 120px;" class = "hidden-sm hidden-md hidden-lg">
+		
+			<div class = row style="text-align: center;">
+				<h6>&#9400 2017 Courtionary.Co</h6>
+			</div>
+
+			<div class = row style="text-align: center; margin-top: 0px;">
+				
+				<h6 style = "margin-top: 1px;"> <a href="#" class = termsandprivacy>Terms</a> | <a href="#" class = termsandprivacy>Privacy</a> </h6>
+			</div>
+		
+	</footer>
+
+	<footer style="margin-top: 260px;" class = "hidden-xs hidden-sm hidden-lg">
+		
+			<div class = row style="text-align: center;">
+				<h6>&#9400 2017 Courtionary.Co</h6>
+			</div>
+
+			<div class = row style="text-align: center; margin-top: 0px;">
+				
+				<h6 style = "margin-top: 1px;"> <a href="#" class = termsandprivacy>Terms</a> | <a href="#" class = termsandprivacy>Privacy</a> </h6>
+			</div>
+		
+	</footer>
+
+		<footer style="margin-top: 300px;" class = "hidden-xs hidden-md hidden-lg">
+		
+			<div class = row style="text-align: center;">
+				<h6>&#9400 2017 Courtionary.Co</h6>
+			</div>
+
+			<div class = row style="text-align: center; margin-top: 0px;">
+				
+				<h6 style = "margin-top: 1px;"> <a href="#" class = termsandprivacy>Terms</a> | <a href="#" class = termsandprivacy>Privacy</a> </h6>
+			</div>
+		
+	</footer>
+
+
+
+
+<script type="text/javascript">
+	document.getElementById("logInButton").addEventListener("mouseover" , hover);
+	function hover () {
+		document.getElementById("logInButton").style.backgroundColor = "#2874A6";
+	}
+	document.getElementById("logInButton").addEventListener("mouseout" , changeBackColor);
+	function changeBackColor () {
+		document.getElementById("logInButton").style.backgroundColor = "#3498DB";
+	}
+</script>
+
+</body>
+</html>
